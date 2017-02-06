7a44a78a601b174abe4d0e7c1a0df8abda6892db
sudhakar-settipalli <sudhakar.settipalli@flipkart.com>
2017-02-06 16:11:15 +0530

    Delete first_mile_bag_shipmment_map_hive_fact.sql

--- first_mile_bag_shipmment_map_hive_fact.sql
+++
@@ -1,7 +0,0 @@
-INSERT OVERWRITE TABLE first_mile_bag_shipmment_map_hive_fact
-SELECT
-`data`.bagid as bag_tracking_id,
-exp as shipment_id,
-min(updatedat) as first_time
-FROM
-bigfoot_snapshot.dart_wsr_scp_ekl_firtmilebag_2_0_view lateral view explode(`data`.shipmentids) exploded_table as exp group by `data`.Bagid, exp ;

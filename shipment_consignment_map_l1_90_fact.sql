eecbb6915c327234f1639072de821f56b6d271ba
sudhakar-settipalli <sudhakar.settipalli@flipkart.com>
2017-02-06 16:02:16 +0530

    Delete shipment_consignment_map_l1_90_fact.sql

--- shipment_consignment_map_l1_90_fact.sql
+++
@@ -1,9 +0,0 @@
-INSERT OVERWRITE TABLE shipment_consignment_map_l1_90_fact
-SELECT entityid AS consignment_id,
-       exp AS shipment_id,
-       min(updatedat) AS first_time
-FROM bigfoot_journal.dart_wsr_scp_ekl_shipmentgroup_3 LATERAL VIEW explode(`data`.shipments) exploded_table AS exp
-WHERE `data`.type = 'consignment' and  day > date_format(date_sub(current_date,100),'yyyyMMdd') and lower(`data`.status) <> 'created'
-GROUP BY entityid,
-         exp;
-
